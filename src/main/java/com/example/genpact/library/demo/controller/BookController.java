fd9cd69a6329551cdd1ff06046b9222c483720cc
jitmathu <jitendra.mathur@publicissapient.com>
2020-06-28 13:33:15 +0530

    CrossOrigin

--- src/main/java/com/example/genpact/library/demo/controller/BookController.java
+++ src/main/java/com/example/genpact/library/demo/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import com.example.genpact.library.demo.service.book.BookService;
 
 @RestController
 @RequestMapping("/book")
+@CrossOrigin("*")
 public class BookController {
 
 	@Autowired
--- src/main/java/com/example/genpact/library/demo/controller/LibraryController.java
+++ src/main/java/com/example/genpact/library/demo/controller/LibraryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import com.example.genpact.library.demo.service.library.LibraryService;
  */
 @RestController
 @RequestMapping("/library")
+@CrossOrigin("*")
 public class LibraryController {
 	
 	@Autowired
